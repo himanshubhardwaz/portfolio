d98727cefa01bed5b5d74f1f2ac9f7b9ec019694
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-06-26 23:54:14 +0530

    ts -> js (wiil look into it later) getResume endpoint

--- src/pages/api/getResume.js
+++ src/pages/api/getResume.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch';
 const pipeline = promisify(stream.pipeline);
 const url = 'https://drive.google.com/uc?id=1buEjiSIHEjc6TfZi51xD6nCKM1WPNLNz&export=download';
 
-const handler = async (req: any, res: { setHeader: (arg0: string, arg1: string) => void; }) => {
+const handler = async (req, res) => {
     const response = await fetch(url); // replace this with your API call & options
     if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 
