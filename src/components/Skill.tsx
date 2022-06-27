28706a2f47b70517770012b7524849b70fbc8af0
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-06-27 10:47:24 +0530

    updated readme

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
 import { FC, } from "react";
 
 type Props = { img: string, name: string }
@@ -8,7 +8,7 @@ const Skill: FC<Props> = ({ img, name }) => {
         <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                 <div className='m-auto'>
-                    <img src={img} width='64px' height='64px' alt='/' />
+                    <Image src={img} width='64px' height='64px' alt='/' />
                 </div>
                 <div className='flex flex-col items-center justify-center'>
                     <h3>{name}</h3>
